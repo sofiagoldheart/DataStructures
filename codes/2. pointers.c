f92c9d5e3a0266e580c842f77836404ca9a5d818
sofia <90588277+sofiagoldheart@users.noreply.github.com>
2022-08-22 14:52:59 -0500

    Update 2. pointers.c

--- codes/2. pointers.c
+++ codes/2. pointers.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
-
-int x,a;
+//Definir las variable entera a y el apuntador y.
+int a;
 int *y;
 
 int main(){
-scanf ("%d", &a);
+//Guardar valor de a con 5 y el de y con dirección de memoria de a.
+a=5;
 y=&a;
-printf("%d",a);
+
+//Imprimir el valor de a y y.
+printf("El valor de la variable es %d\n",a);
+printf("El valor del puntero es %p",y);
 
 return 0;
 }
